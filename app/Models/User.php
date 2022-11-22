a715bad771fdcc6e907b11f621bfcddedf1c67fe
Alsu <gabdrahimovaalsy@gmail.com>
2022-11-23 00:11:59 +0500

    Register Login Logout (2)

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,10 +35,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -60,6 +62,9 @@ class User extends Authenticatable
 //        'password',
 //        'remember_token',
 //    ];
+
+    public $timestamps = false;
+
     protected $guarded = false;
     /**
      * The attributes that should be cast.
