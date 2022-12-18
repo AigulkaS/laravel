091199948b5cbc1ab33291ce39cd5d64273a8b5d
Lili Petrik <lilipetrikapps@gmail.com>
2022-12-18 22:50:50 +0500

    correct role controller

--- app/Http/Controllers/Today/EditController.php
+++ app/Http/Controllers/Today/EditController.php
@@ -15,28 +15,24 @@ class EditController extends BaseController
     {
         $data = $request->validated();
 
-        // $roleData = [
-        //     'name' => 'surgeon',
-        // ];
-        // $filter = app()->make(RoleFilter::class, ['queryParams' => array_filter($roleDat)]);
-        // $surgeonRoles = Role::filter($filter)->get();
-        // $data['role_id'] = $surgeonRoles[0]->id; 
+        $roleData = [
+            'name' => 'surgeon',
+        ];
+        $filter = app()->make(RoleFilter::class, ['queryParams' => array_filter($roleData)]);
+        $surgeonRoles = Role::filter($filter)->get();
+        $data['role_id'] = $surgeonRoles[0]->id; 
 
-        //! role_id = 1 it's surgeon
-        $data['role_id'] = 1; 
         $filter = app()->make(UserFilter::class, ['queryParams' => array_filter($data)]);
         $surgeons = User::filter($filter)->get();
 
 
-        // $roleData = [
-        //     'name' => 'cardiologist',
-        // ];
-        // $filter = app()->make(RoleFilter::class, ['queryParams' => array_filter($roleDat)]);
-        // $cardiologistRoles = Role::filter($filter)->get();
-        // $data['role_id'] = $cardiologistRoles[0]->id; 
+        $roleData = [
+            'name' => 'cardiologist',
+        ];
+        $filter = app()->make(RoleFilter::class, ['queryParams' => array_filter($roleData)]);
+        $cardiologistRoles = Role::filter($filter)->get();
+        $data['role_id'] = $cardiologistRoles[0]->id; 
 
-        //! role_id = 2 it's cardiologist
-        $data['role_id'] = 2; 
         $filter = app()->make(UserFilter::class, ['queryParams' => array_filter($data)]);
         $cardiologist = User::filter($filter)->get();
 
